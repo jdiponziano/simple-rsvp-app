53f2d4a595ba690e5c77895fa0262362e693bd53
Jessica Di Ponziano <jdiponziano@gmail.com>
2019-01-05 16:01:41 -0500

    Refactor create li function

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ An RSVP App
 ******************************************/
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+
   const form = document.getElementById('registrar');
   const input = form.querySelector('input');
   const mainDiv = document.querySelector('.main');
@@ -39,22 +39,24 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   const createLI = text => {
+    function createElement(elementName, property, value) {
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element;
+    }
+
+    function appendToLI(elementName, property, value) {
+      const element = createElement(elementName, property, value);
+      li.appendChild(element);
+      return element;
+    }
+
     const li = document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = text;
-    li.appendChild(span);
-    const label = document.createElement('label');
-    label.textContent = 'Confirmed';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    label.appendChild(checkbox);
-    li.appendChild(label);
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Edit';
-    li.appendChild(editButton);
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    li.appendChild(removeButton);
+    appendToLI('span', 'textContent', text);
+    appendToLI('label', 'textContent', 'Confirmed')
+      .appendChild(createElement('input', 'type', 'checkbox'));
+    appendToLI('button', 'textContent', 'Edit');
+    appendToLI('button', 'textContent', 'Remove');
     return li;
   };
 
